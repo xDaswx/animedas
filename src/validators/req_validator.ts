9661cface5743883661058dd107218c67e4c5acb
sirguy12 <ryankairo09@yahoo.com.br>
2023-05-10 21:40:54 -0300

    validator update

--- src/validators/req_validator.ts
+++ src/validators/req_validator.ts
@@ -1,11 +1,13 @@
 import {Request,Response, NextFunction} from 'express';
 
 const fieldvalidator = (req:Request, res:Response ,next:NextFunction) =>{
-    if (!req.body.title) {
-        return res.status(400).send({message:'Não valido'})
+
+    const {url,description,type} = req.body
+
+    if (!url || !description || !type) {
+        return res.status(400).send({message:'Requesição não foi aceita pois não foi informado as fields necessárias', missing: ["url","description","type"]})
     }
     next()
 }
-
 
 export default fieldvalidator;
