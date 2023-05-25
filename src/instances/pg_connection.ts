7288aab2236a79c2be3ac4b2e2c00783b165d1f8
sirguy12 <ryankairo09@yahoo.com.br>
2023-05-25 17:30:52 -0300

    fix: sequelize then catch handler

--- src/instances/pg_connection.ts
+++ src/instances/pg_connection.ts
@@ -13,10 +13,10 @@ export const sequelize = new Sequelize(
         port: 5432
     }
 )
-try{
-    sequelize.authenticate()
-    console.log('Autenticado ao banco de dados')
-}
-catch(error){
-    console.log('Erro ao se conectar ao banco de dados', error)    
-}
+
+sequelize
+.authenticate()
+.then(()=>{console.log('Conexão ao banco de dados bem sucedida')})
+.catch((error) =>{
+    console.log('Erro ao se conectar no banco de dados: ', error)
+})
