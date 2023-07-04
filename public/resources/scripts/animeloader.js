2306b0783d0ba93717ffb8eb25f69e5e8a1f7391
sirguy12 <ryankairo09@yahoo.com.br>
2023-07-04 16:50:44 -0300

    added a tag to href works

--- public/resources/scripts/animeloader.js
+++ public/resources/scripts/animeloader.js
@@ -8,16 +8,23 @@ fetch(url+'/api/v1/many/random', {
     return response.json()
 }).then((json)=> {
     const divanime = document.querySelector('.api-images')
+    
     for (animeimg of json.content) {
 
         let newdiv = document.createElement('div');
         newdiv.classList.add('image-loaded');
 
+        let clickable =  document.createElement('a')
+        clickable.setAttribute('target','_blank')
+        clickable.setAttribute('href','/see/'+animeimg.id)
+
         let new_image = document.createElement('img');
         new_image.classList.add('anime')
         new_image.src = animeimg.url
 
-        newdiv.append(new_image)
+        clickable.append(new_image)
+        newdiv.append(clickable)
         divanime.append(newdiv)
 }
-})
+}).then(()=> document.querySelectorAll('.anime').forEach((anime)=>{anime.addEventListener('click',(html_information)=> console.log("clicou em "+html_information))})
+)
