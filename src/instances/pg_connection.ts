e6ed5de82eb30d661dad717ca1afd20a64b1215a
sirguy12 <69166890+xDaswx@users.noreply.github.com>
2024-09-26 15:02:33 -0300

    [fix] database connection fix

--- src/instances/pg_connection.ts
+++ src/instances/pg_connection.ts
@@ -3,16 +3,7 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const sequelize = new Sequelize(
-    process.env.DATABASE as string,
-    process.env.USER as string,
-    process.env.PASSWORD as string,
-    {
-        host: process.env.HOST as string,
-        dialect: 'postgres',
-        port: 7018
-    }
-)
+export const sequelize = new Sequelize(process.env.DB_URL as string)
 
 sequelize
 .authenticate()
